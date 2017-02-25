53bae2625905e0dc53a38f7b0806a1b77ab09a86
Jakob Fix <jakob@jfix.com>
2017-02-25 23:08:26 +0100

    :tada: First commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const fs = require('fs')
+
+exports.randomFile = function (dir, callback) {
+  fs.readdir(dir, (err, files) => {
+    if (err) return callback(err)
+
+    function checkRandom () {
+      if (!files.length) {
+        // callback with an empty string to indicate there are no files
+        return callback(null, undefined)
+      }
+      const randomIndex = Math.floor(Math.random() * files.length)
+      const file = files[randomIndex]
+      fs.stat(path.join(dir, file), (err, stats) => {
+        if (err) return callback(err)
+        if (stats.isFile()) {
+          return callback(null, file)
+        }
+        // remove this file from the array because for some reason it's not a file
+        files.splice(randomIndex, 1)
+
+        // try another random one
+        checkRandom()
+      })
+    }
+    checkRandom()
+  })
+}
---
+++ test.js
@@ -0,0 +1,73 @@
+const fs = require('fs')
+const rimraf = require('rimraf')
+const async = require('async')
+const assert = require('chai').assert
+const path = require('path')
+
+const rf = require('./index.js')
+
+const baseDir = '/tmp/test-dir'
+
+// TODO: clean up this callback hell
+
+describe('randomFile', function () {
+  it('should throw an error if directory does not exist', function (done) {
+    const testDir = path.join(baseDir, 'i-dont-exist')
+    assert.throws(
+      () => {
+        rf.randomFile(testDir, done)
+        done()
+      },
+      /ENOENT: no such file or directory/,
+      'The directory does not exist'
+    )
+  })
+
+  it('should return undefined if no files in directory', function (done) {
+    fs.mkdtemp(baseDir, (err, d) => {
+      if (err) done(err)
+      rf.randomFile(d, (err, file) => {
+        if (err) return done(err)
+        assert.equal(undefined, file)
+        rimraf(d, () => done())
+      })
+    })
+  })
+
+  it('should return one file', function (done) {
+    const fn = 'file-a'
+    fs.mkdtemp(baseDir, (err, d) => {
+      if (err) return done(err)
+
+      fs.writeFile(path.join(d, fn), (err, f) => {
+        if (err) return done(err)
+
+        rf.randomFile(d, (err, file) => {
+          if (err) return done(err)
+
+          assert.equal(fn, file)
+          rimraf(d, () => done())
+        })
+      })
+    })
+  })
+
+  it('should return one of several files', function (done) {
+    const files = ['file-a', 'file-b', 'file-c', 'file-d']
+    fs.mkdtemp(baseDir, (err, d) => {
+      if (err) return done(err)
+
+      async.each(files, function (file, done) {
+        fs.writeFile(path.join(d, file), (err, f) => {
+          if (err) return done(err)
+        })
+      })
+
+      rf.randomFile(d, (err, file) => {
+        if (err) return done(err)
+        assert.include(files, file)
+        rimraf(d, () => done())
+      })
+    })
+  })
+})
