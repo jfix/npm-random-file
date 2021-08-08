5bfafb537c422efeb4ff96435c12499278d0bd57
iComputer7 <6298231+icomputer7@users.noreply.github.com>
2021-08-08 01:26:19 +0000

    Revising TypeScript definitions

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,8 @@
-export default function(dir: string, callback: (err: NodeJS.ErrnoException | null, file?: string) => void): void
+declare namespace randomFile {
+    interface randomFile {
+        (dir: string, callback: (err: NodeJS.ErrnoException | null, file?: string) => void): void
+    }
+}
+
+declare var randomFile: randomFile.randomFile;
+export = randomFile;
